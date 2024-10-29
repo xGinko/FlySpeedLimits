9fed65a78920aa8e88b8f6f968456926cb7edc85
xGinko <rachel.audrey@mail.de>
2024-10-29 22:13:39 +0100

    add suffix shortcut

--- src/main/java/me/xginko/flyspeedlimits/struct/SpeedUnit.java
+++ src/main/java/me/xginko/flyspeedlimits/struct/SpeedUnit.java
@@ -4,20 +4,30 @@ import me.xginko.flyspeedlimits.FlySpeedLimits;
 
 public enum SpeedUnit {
 
-    BLOCKS_PER_TICK {
+    BLOCKS_PER_TICK("bpt") {
         public double fromDistance(double rawDist) {
             return toBlocksPerMillis(rawDist, 50);
         }
     },
 
-    BLOCKS_PER_SECOND {
+    BLOCKS_PER_SECOND("bps") {
         public double fromDistance(double rawDist) {
             return toBlocksPerMillis(rawDist, 1000);
         }
     };
 
+    private final String suffix;
+
+    SpeedUnit(String suffix) {
+        this.suffix = suffix;
+    }
+
     public double fromDistance(double rawDist) {
         throw new AbstractMethodError();
+    }
+
+    public String getSuffix() {
+        return suffix;
     }
 
     private static double toBlocksPerMillis(double blocks, long millis) {
